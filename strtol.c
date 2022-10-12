adc67be516c50a3078b4010ee6e5503c8fa0219a
Maurice <75327790+F4hdC@users.noreply.github.com>
2022-10-12 23:15:54 +0200

    improving strtol with error handling

--- strtol.c
+++ strtol.c
@@ -4,11 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main(int argc, char* argv[]){
+int main(int argc, char* argv[]){
 
 	char *ptr;
-
-	int num = strtol(argv[1],&ptr,10);
-	printf("%d\n",num);
+	if ( argc > 1 ) {
+		int num = strtol(argv[1],&ptr,10);
+		// habrá que verificar que ptr retorna un 0, si no, eso quiere decir que la cadena no es convertible a un Int, exmp : "123String" "string"
+		if (*ptr == 0) printf("%d\n",num);
+		else printf("Input erroneo \n");
+	}else printf("Sin Entrada \n");
 
 }
